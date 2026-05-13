e8e9f5fe85046319284a6f1f7d16c63b2a986f31
vikkisupurrbe <yingjialiu1216@gmail.com>
2026-05-13 16:11:16 -0700

    but with fetch weather

--- week-02/day-01/fetch-data/fetchJokes.js
+++ week-02/day-01/fetch-data/fetchJokes.js
@@ -4,23 +4,45 @@ Build a script that fetches data from 3 public APIs (jokes, weather, quotes),
 displays combined results, 
 then refactor with Promise.all to run in parallel and compare timing
 
+Example of the response object:
+
+Response {
+  status: 200,
+  statusText: 'OK',
+  headers: Headers {
+    'content-type': 'application/json; charset=utf-8',
+    vary: 'Accept-Encoding',
+    'x-powered-by': 'Express',
+    'access-control-allow-origin': '*',
+    etag: 'W/"6b-LybmR4RoX0i054YNqKPD6EiE9Qs"',
+    'content-encoding': 'gzip',
+    'x-cloud-trace-context': 'e16af9c328743d1a7927aa0daf4f1c98',
+    date: 'Wed, 13 May 2026 21:12:50 GMT',
+    server: 'Google Frontend',
+    'alt-svc': 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000',
+    'transfer-encoding': 'chunked'
+  },
+  body: ReadableStream { locked: false, state: 'readable', supportsBYOB: true },
+  bodyUsed: false,
+  ok: true,
+  redirected: false,
+  type: 'basic',
+  url: 'https://official-joke-api.appspot.com/random_joke'
+}
+
 */
 
 function fetchJokes() {
   const url = "https://official-joke-api.appspot.com/random_joke";
-  const response = fetch(url);
-
-  return new Promise ((resolve, reject) => {
-    if (response) {
-      resolve(response);
-    } else {
-      reject({
-        name: "Error!"
-      })
-    }
-  })
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json();
+    })
 }
 
-fetchJokes().then((message) => {
-  console.log(message);
-}).catch((error) => console.log(error.name));
+fetchJokes()
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
--- week-02/day-01/fetch-data/fetchWeather.js
+++ week-02/day-01/fetch-data/fetchWeather.js
@@ -5,3 +5,29 @@ displays combined results,
 then refactor with Promise.all to run in parallel and compare timing
 
 */
+
+import { fetchWeatherApi } from "openmeteo";
+
+function fetchWeather() {
+  const params = {
+    latitude: 49.28,
+    longitude: -123.12,
+    hourly: "temperature_2m",
+    timezone: "America/Los_Angeles",
+    forecast_days: 1,
+  }
+  
+  const url = "https://api.open-meteo.com/v1/forecast";
+
+  fetchWeatherApi(url, params)
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error (`HTTP error! Status: ${response.status}`);
+      }
+      return response[0];
+    })
+
+}
+
+fetchWeather()
+  .then(data => console.log(data));
