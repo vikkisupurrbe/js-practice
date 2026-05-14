086c3840c402417791057bff055176b3c652df85
vikkisupurrbe <yingjialiu1216@gmail.com>
2026-05-13 17:25:03 -0700

    refactor logic in fetch weather

--- week-02/day-01/fetch-data/fetchWeather.js
+++ week-02/day-01/fetch-data/fetchWeather.js
@@ -19,38 +19,31 @@ function fetchWeather() {
   }
 
   return fetchWeatherApi(url, params)
-    .then(response => response[0]);
+    .then(response => {
+      const data = response[0];
+      const utcOffsetSeconds = data.utcOffsetSeconds();
+      const hourly = data.hourly();
+      const weatherData = {
+        hourly: {
+          time: Array.from(
+            { length: (Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval() },
+            (_, i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
+          ),
+          temperature_2m: hourly.variables(0).valuesArray(),
+        },
+      };
+      return {
+        latitude: data.latitude(),
+        longitude: data.longitude(),
+        elevation: data.elevation(),
+        timezone: data.timezone(),
+        timezoneAbbreviation: data.timezoneAbbreviation(),
+        hourlydata: weatherData.hourly
+      }
+    });
 
 }
 
 fetchWeather()
-  .then((data) => {
-    const latitude = data.latitude();
-    const longitude = data.longitude();
-    const elevation = data.elevation();
-    const timezone = data.timezone();
-    const timezoneAbbreviation = data.timezoneAbbreviation();
-    const utcOffsetSeconds = data.utcOffsetSeconds();
-
-    console.log(
-      `\nCoordinates: ${latitude}°N ${longitude}°E`,
-      `\nElevation: ${elevation}m asl`,
-      `\nTimezone: ${timezone} ${timezoneAbbreviation}`,
-      `\nTimezone difference to GMT+0: ${utcOffsetSeconds}s`,
-    );
-
-    const hourly = data.hourly();
-
-    const weatherData = {
-      hourly: {
-        time: Array.from(
-          { length: (Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval() },
-          (_, i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
-        ),
-        temperature_2m: hourly.variables(0).valuesArray(),
-      },
-    };
-
-    console.log("\nHourly data:\n", weatherData.hourly)
-  })
+  .then((data) => console.log(data))
   .catch(error => console.log(error));
