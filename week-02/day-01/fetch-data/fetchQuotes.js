c47f9e1c80e4de02ce94d2f8c1077894dde9b0e5
vikkisupurrbe <yingjialiu1216@gmail.com>
2026-05-13 17:12:55 -0700

    implement logic to fetch quotes

--- week-02/day-01/fetch-data/fetchQuotes.js
+++ week-02/day-01/fetch-data/fetchQuotes.js
@@ -5,3 +5,17 @@ displays combined results,
 then refactor with Promise.all to run in parallel and compare timing
 
 */
+
+function fetchQuotes() {
+  const url = "https://quotesapi.prayushadhikari.com.np/api/quotes/random";
+  return fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+}
+
+fetchQuotes()
+  .then(response => console.log(response.data[0]));
