cf430a9b255964c8e685e395989e3f5731135545
vikkisupurrbe <yingjialiu1216@gmail.com>
2026-05-15 22:13:57 -0700

    set up todo list

---
+++ week-02/day-03/to-do-list/todoList.js
@@ -0,0 +1,10 @@
+/*
+
+Build an interactive to-do list using vanilla JS + DOM: 
+  - add, 
+  - delete, 
+  - mark complete, 
+  - filter by status, 
+  - persist to localStorage. No frameworks.
+
+*/
