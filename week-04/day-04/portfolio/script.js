49cf9528110dd836abc3ec35ebbff120c766bef4
vikkisupurrbe <yingjialiu1216@gmail.com>
2026-07-22 17:51:57 -0700

    add close dropdown by clicking elsewhere

--- week-04/day-04/portfolio/script.js
+++ week-04/day-04/portfolio/script.js
@@ -1,8 +1,30 @@
 const toggle = document.getElementById("nav-toggle");
 const links = document.getElementById("navbar-links");
+const navbar = document.querySelector(".navbar");
 
-toggle.addEventListener("click", () => {
-  const isOpen = links.classList.toggle("is-open");
-  toggle.setAttribute("aria-expanded", isOpen);
-  document.body.classList.toggle("nav-open", isOpen);
+function openMenu() {
+  links.classList.add("is-open");
+  toggle.setAttribute("aria-expanded", "true");
+  document.body.classList.add("nav-open");
+}
+
+function closeMenu() {
+  links.classList.remove("is-open");
+  toggle.setAttribute("aria-expanded", "false");
+  document.body.classList.remove("nav-open");
+}
+
+/* Clicking the dropdown button */
+toggle.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const isOpen = links.classList.contains("is-open");
+  isOpen ? closeMenu() : openMenu();
 })
+
+/* Clicking outside the dropdown menu and the navbar */
+document.addEventListener("click", (e) => {
+  const isOpen = links.classList.contains("is-open");
+  if (isOpen && !navbar.contains(e.target)) {
+    closeMenu();
+  }
+});
